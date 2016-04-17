b679860f004d0a3635cd1bb0262a6bb0dc1bcaef
jyyy410 <84795730@qq.com>
2016-04-17 14:41:00 +0800

    格式化

--- src/tempVo/About.java
+++ src/tempVo/About.java
@@ -1,18 +1,19 @@
 package tempVo;
 
-   /**
-    * about 实体类
-    * 2016-27-15 靳阳阳
-    */ 
+/**
+ * about 实体类
+ * 2016-27-15 靳阳阳
+ */
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 @Entity
 @Table(name = "about")
-public class About{
+public class About {
 
 	private Integer ID;
 
@@ -28,56 +29,68 @@ public class About{
 
 	private String data;
 
-@Id
-@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "ID")
-	public Integer getID(){
+	public Integer getID() {
 		return ID;
 	}
-	public void setID(Integer ID){
-	this.ID=ID;
+
+	public void setID(Integer ID) {
+		this.ID = ID;
 	}
+
 	@Column(name = "title")
-	public String getTitle(){
+	public String getTitle() {
 		return title;
 	}
-	public void setTitle(String title){
-	this.title=title;
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
+
 	@Column(name = "entitle")
-	public String getEntitle(){
+	public String getEntitle() {
 		return entitle;
 	}
-	public void setEntitle(String entitle){
-	this.entitle=entitle;
+
+	public void setEntitle(String entitle) {
+		this.entitle = entitle;
 	}
+
 	@Column(name = "content")
-	public String getContent(){
+	public String getContent() {
 		return content;
 	}
-	public void setContent(String content){
-	this.content=content;
+
+	public void setContent(String content) {
+		this.content = content;
 	}
+
 	@Column(name = "encontent")
-	public String getEncontent(){
+	public String getEncontent() {
 		return encontent;
 	}
-	public void setEncontent(String encontent){
-	this.encontent=encontent;
+
+	public void setEncontent(String encontent) {
+		this.encontent = encontent;
 	}
+
 	@Column(name = "paixu")
-	public Integer getPaixu(){
+	public Integer getPaixu() {
 		return paixu;
 	}
-	public void setPaixu(Integer paixu){
-	this.paixu=paixu;
+
+	public void setPaixu(Integer paixu) {
+		this.paixu = paixu;
 	}
+
 	@Column(name = "data")
-	public String getData(){
+	public String getData() {
 		return data;
 	}
-	public void setData(String data){
-	this.data=data;
+
+	public void setData(String data) {
+		this.data = data;
 	}
 }
-
