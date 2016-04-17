7599f7e2ee01f7a3978e79c43c9a5b52f35b8a3b
jyyy410 <84795730@qq.com>
2016-04-17 22:33:34 +0800

    1

---
+++ src/com/jeecms/until/CookiesUtils.java
@@ -0,0 +1,18 @@
+package com.jeecms.until;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public class CookiesUtils {
+public static String getCookieValueByName(HttpServletRequest request,String cookieName) {
+	String cookieName0="";
+	Cookie[] cookies = request.getCookies();
+	for (Cookie cookie : cookies) {
+		if (cookie.getName().equals(cookieName)) {
+			cookieName0 = cookie.getValue();
+		}
+	}
+	return cookieName0;
+	
+}
+}
