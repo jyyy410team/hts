a3a2e8b4bddf8b1044c145ddcc0cb9e2a65aef7e
jyyy410 <847945730@qq.com>
2016-04-27 15:13:19 +0800

    1

--- src/com/jeecms/cms/action/BaseAction.java
+++
@@ -1,9 +0,0 @@
-package com.jeecms.cms.action;
-
-import org.springframework.orm.hibernate3.HibernateTemplate;
-
-public class BaseAction extends HibernateTemplate {
- public static void main(String[] args) {
-
-}
-}
